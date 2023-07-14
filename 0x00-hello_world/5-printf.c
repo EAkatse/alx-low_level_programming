93cdabb5123cc4a26ce85ecc6e94ac878d32598b
EAkatse <e.akatse@gmail.com>
2023-07-13 17:06:20 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: A program that returns a line and returns a value of 0
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf(“with proper grammar, but the outcome is a piece of art, \n”);
 	return (0);
 }
