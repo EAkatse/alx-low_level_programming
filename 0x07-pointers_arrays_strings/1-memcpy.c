2c9f05a82c3860a5e82c4e75798215177aba8e7c
EAkatse <e.akatse@gmail.com>
2023-07-31 20:07:01 -0700

    0x07. C - Even more pointers, arrays and strings (tasks 1, 3, 7, and 8 corrected

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,8 +10,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (x = 0; x < n; x++)
-		*(dest + x) =  *(src + x);
+	for (i = 0; i < n; i++)
+		*(dest + i++) =  *(src + i);
 
 	return (dest);
 }
