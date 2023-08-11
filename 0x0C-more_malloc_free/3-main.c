9fa48fb57c4a457c77fb22604aee2b26297c3246
EAkatse <e.akatse@gmail.com>
2023-08-10 18:11:11 -0700

    3. array_range

---
+++ 0x0C-more_malloc_free/3-main.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+    unsigned int n;
+
+    n = 0;
+    while (n < size)
+    {
+        if (n % 10)
+        {
+            printf(" ");
+        }
+        if (!(n % 10) && n)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[n]);
+        n++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int *a;
+
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
+}
